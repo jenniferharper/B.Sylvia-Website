ffc882f7c7ce6a22f1fa5c15ec3de07a992c23a4
jenniferHarper <52581347+jenniferharper@users.noreply.github.com>
2021-08-13 14:42:55 +0200

    with preloader

--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
 gsap.registerPlugin(ScrollTrigger);
+
+
+
+////////// slider
+var container = $('.slider .wrapper');
+var boxes = document.querySelectorAll('.section');
+
+let tlSlider = gsap.timeline({
+	scrollTrigger: {
+		trigger: ".slider",
+		start: "center center-=100",
+		end: function(){  
+			return "+=" + container[0].scrollWidth;
+		},
+		scrub: true,
+		pin: true,
+		toggleClass: "is-active",
+		refreshPriority: 1,
+	}
+});
+
+refresher();
+function refresher(){
+	gsap.set(boxes, {clearProps:"all"}); 
+	tlSlider.clear();
+	tlSlider.to(boxes, {	
+		x: function(){  
+		return -(container[0].scrollWidth - document.documentElement.clientWidth) + "px";
+		},
+	});
+};
+
+
+
 //---------- Navigation
 //////hamburger and menu changes/////
 $('.navbar-toggler').click(function(){
@@ -35,34 +69,7 @@ onUpdate: ({direction}) => {
 }}
 });
 
-////////// slider
-var container = $('.slider .wrapper');
-var boxes = document.querySelectorAll('.section');
 
-let tlSlider = gsap.timeline({
-	scrollTrigger: {
-		trigger: ".slider",
-		start: "center center-=100",
-		end: function(){  
-			return "+=" + container[0].scrollWidth;
-		},
-		scrub: true,
-		pin: true,
-		toggleClass: "is-active",
-		refreshPriority: 1,
-	}
-});
-
-refresher();
-function refresher(){
-	gsap.set(boxes, {clearProps:"all"}); 
-	tlSlider.clear();
-	tlSlider.to(boxes, {		
-		x: function(){  
-		return -(container[0].scrollWidth - document.documentElement.clientWidth) + "px";
-		},
-	});
-};
 
 
 
@@ -117,7 +124,6 @@ gsap.utils.toArray('.up-text').forEach(section => {
 		trigger: section,
 		start: 'top bottom+=100',
 		toggleActions: 'play none none none',
-
 		}
 	});
 });
@@ -135,3 +141,59 @@ $(window).resize(function(){
 	refresher();	
 	}
 });
+
+
+///////////////////
+
+var imgLoad = imagesLoaded('.overflow-wrapper');
+
+var progressBar = $(".c-preloader__progress"),
+    count = $(".c-preloader__count"),
+    images = $("img").length,
+    loadedCount = 0,
+    loadingProgress = 0,
+    tlProgress = new TimelineMax();
+ 
+imgLoad.on( 'progress', function( instance, image ) {
+    loadProgress();
+});
+ 
+function loadProgress(imgLoad, image) {
+
+    loadedCount++;
+  
+    loadingProgress = (loadedCount/images);
+    console.log(loadingProgress);
+
+    TweenMax.to(tlProgress, 1, {progress:loadingProgress});
+}
+
+var tlProgress = new TimelineMax({
+    paused: true,
+    onUpdate: countPercent,
+    onComplete: loadComplete
+});
+ 
+tlProgress
+	.to('html',{overflow:'hidden'},0)
+    .to(progressBar, 1, {width:"100%"},0)
+	.set('.hero', {yPercent:100},0)
+	.to(count, 0.5, {autoAlpha:0},1)
+
+
+function countPercent() {
+      var newPercent = (tlProgress.progress()*100).toFixed();
+      count.text(newPercent + "%");
+}
+
+function loadComplete() {
+  var tlEnd = new TimelineMax({
+	// onComplete: loadCompleteOther
+  });
+  tlEnd
+      .to('html',{overflow:'visible'},0)
+      .to(".c-preloader", 0.5, { yPercent:-100, opacity:0, transformOrigin: "center top"},0)
+	  .to('.hero',0.5,  {yPercent:0},'-=0.5')
+}
+
+
